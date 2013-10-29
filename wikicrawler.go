c57ca401d8941a7b5435a604e7c77391749a5cae
aliang <a.liang1989@gmail.com>
2013-10-28 19:08:50 -0700

    adding placeholder for img

--- wikicrawler.go
+++ wikicrawler.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-    "github.com/PuerkitoBio/goquery"
-    "io/ioutil"
-    "log"
-    "net/url"
-    "os"
-    "strconv"
-    "strings"
-    "sync"
+	"github.com/PuerkitoBio/goquery"
+	"io/ioutil"
+	"log"
+	"net/url"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
 )
 
 var fmt = log.New(os.Stdout, "", 0)
@@ -18,26 +18,26 @@ var fmt = log.New(os.Stdout, "", 0)
  * the 0600 permissions
  */
 func WriteToFile(relativeUrl string, directory string, content string) {
-    if strings.TrimSpace(directory) == "" {
-        directory = "."
-    }
+	if strings.TrimSpace(directory) == "" {
+		directory = "."
+	}
 
-    pathname := strings.Join([]string{directory, relativeUrl}, "")
-    // fmt.Println("path: %s", pathname)
-    err := ioutil.WriteFile(pathname, []byte(content), 0777)
-    if err != nil { //the parent directory doesn't exist on the filesystem
-        //get our filename
-        //this isn't pretty, but it works for now. TODO: look if there's a better function for doing this
-        splits := strings.Split(relativeUrl, "/")
-        filename := splits[len(splits)-1]
-        newpath := strings.TrimSuffix(pathname, filename)
-        os.MkdirAll(newpath, 0777)
+	pathname := strings.Join([]string{directory, relativeUrl}, "")
+	// fmt.Println("path: %s", pathname)
+	err := ioutil.WriteFile(pathname, []byte(content), 0777)
+	if err != nil { //the parent directory doesn't exist on the filesystem
+		//get our filename
+		//this isn't pretty, but it works for now. TODO: look if there's a better function for doing this
+		splits := strings.Split(relativeUrl, "/")
+		filename := splits[len(splits)-1]
+		newpath := strings.TrimSuffix(pathname, filename)
+		os.MkdirAll(newpath, 0777)
 
-        //try writing again
-        fmt.Println("writing file <%s>", pathname)
-        ioutil.WriteFile(pathname, []byte(content), 0777)
-        //todo error handle + output
-    }
+		//try writing again
+		fmt.Println("writing file <%s>", pathname)
+		ioutil.WriteFile(pathname, []byte(content), 0777)
+		//todo error handle + output
+	}
 }
 
 /*
@@ -45,45 +45,49 @@ func WriteToFile(relativeUrl string, directory string, content string) {
  * followed by the links in the page (on the same domain)
  */
 func Scrape(target string, hosturi string) (string, map[string]bool) {
-    defer func() {
-        if r := recover(); r != nil {
-            fmt.Println("panic: %s", r)
-        }
-    }()
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("panic: %s", r)
+		}
+	}()
 
-    // fmt.Println("going to %s", target)
+	// fmt.Println("going to %s", target)
 
-    // Load the HTML document
-    var doc *goquery.Document
-    var e error
+	// Load the HTML document
+	var doc *goquery.Document
+	var e error
 
-    if doc, e = goquery.NewDocument(target); e != nil {
-        panic(e.Error())
-    }
+	if doc, e = goquery.NewDocument(target); e != nil {
+		panic(e.Error())
+	}
 
-    linkSet := make(map[string]bool)
+	linkSet := make(map[string]bool)
 
-    //TODO: on the leaf nodes, won't need to call this
-    doc.Find("a").Each(func(i int, s *goquery.Selection) {
-        link, _ := s.Attr("href")
+	//TODO: on the leaf nodes, won't need to call this
+	doc.Find("a").Each(func(i int, s *goquery.Selection) {
+		link, _ := s.Attr("href")
 
-        if strings.HasSuffix(strings.ToUpper(link), ".JPG") {
-            return
-        }
+		if strings.HasSuffix(strings.ToUpper(link), ".JPG") {
+			return
+		}
 
-        //for now, only accept links on the same domain
-        if strings.HasPrefix(link, "//") {
-            linkSet["http:"+link] = true
-        } else if strings.HasPrefix(link, "/") {
-            linkSet[hosturi+link] = true
-        } else if strings.HasPrefix(link, hosturi) {
-            linkSet[link] = true
-        }
-    })
+		//for now, only accept links on the same domain
+		if strings.HasPrefix(link, "//") {
+			linkSet["http:"+link] = true
+		} else if strings.HasPrefix(link, "/") {
+			linkSet[hosturi+link] = true
+		} else if strings.HasPrefix(link, hosturi) {
+			linkSet[link] = true
+		}
+	})
 
-    html, _ := doc.Html()
+	doc.Find("img").Each(func(i int, s *goquery.Selection) {
+		link, _ := s.Attr("src")
+	})
 
-    return html, linkSet
+	html, _ := doc.Html()
+
+	return html, linkSet
 }
 
 /*
@@ -91,65 +95,65 @@ func Scrape(target string, hosturi string) (string, map[string]bool) {
  * links are followed by spawning goroutines
  */
 func CrawlHandler(url string, depth int, hosturi string) {
-    m := map[string]bool{url: true}
-    var mx sync.Mutex
-    var wg sync.WaitGroup
-    var c2 func(string, int)
-    c2 = func(url string, depth int) {
-        defer wg.Done()
-        if depth <= 0 {
-            return
-        }
-        body, urls := Scrape(url, hosturi)
+	m := map[string]bool{url: true}
+	var mx sync.Mutex
+	var wg sync.WaitGroup
+	var c2 func(string, int)
+	c2 = func(url string, depth int) {
+		defer wg.Done()
+		if depth <= 0 {
+			return
+		}
+		body, urls := Scrape(url, hosturi)
 
-        //get rid of the http://
-        trimmed_url := strings.TrimPrefix(url, "http://")
+		//get rid of the http://
+		trimmed_url := strings.TrimPrefix(url, "http://")
 
-        //put it in a directory - TODO: we'll want to allow the user to specify the output directory
-        WriteToFile(trimmed_url, "./output/", body)
+		//put it in a directory - TODO: we'll want to allow the user to specify the output directory
+		WriteToFile(trimmed_url, "./output/", body)
 
-        mx.Lock()
-        for u, _ := range urls {
-            if !m[u] {
-                m[u] = true
-                wg.Add(1)
-                go c2(u, depth-1)
-            }
-        }
-        mx.Unlock()
-    }
-    wg.Add(1)
-    c2(url, depth)
-    wg.Wait()
+		mx.Lock()
+		for u, _ := range urls {
+			if !m[u] {
+				m[u] = true
+				wg.Add(1)
+				go c2(u, depth-1)
+			}
+		}
+		mx.Unlock()
+	}
+	wg.Add(1)
+	c2(url, depth)
+	wg.Wait()
 }
 
 func main() {
-    defer func() {
-        if r := recover(); r != nil {
-            fmt.Println(r)
-        }
-    }()
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println(r)
+		}
+	}()
 
-    // handle args
-    // TODO: arguments should be a little more robust
-    if len(os.Args[1:]) != 2 {
-        panic("improper usage.\n args[0] = url; args[1] = depth \n ex. go run wikicrawler.go www.google.com 3")
-    }
+	// handle args
+	// TODO: arguments should be a little more robust
+	if len(os.Args[1:]) < 2 {
+		panic("improper usage.\n args[0] = url; args[1] = depth \n ex. go run wikicrawler.go www.google.com 3")
+	}
 
-    target := os.Args[1]
-    depth, err := strconv.Atoi(os.Args[2])
-    if err != nil {
-        panic("improper usage.\n depth (args[1]) should be an integer > 0")
-    }
+	target := os.Args[1]
+	depth, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		panic("improper usage.\n depth (args[1]) should be an integer > 0")
+	}
 
-    u, err := url.Parse(target)
-    if err != nil {
-        panic("improper usage.\n url (args[0]) is not accessible")
-    }
+	u, err := url.Parse(target)
+	if err != nil {
+		panic("improper usage.\n url (args[0]) is not accessible")
+	}
 
-    hosturi := u.Scheme + "://" + u.Host
+	hosturi := u.Scheme + "://" + u.Host
 
-    CrawlHandler(target, depth, hosturi)
-    // Scrape(target)
-    fmt.Println("done")
+	CrawlHandler(target, depth, hosturi)
+	// Scrape(target)
+	fmt.Println("done")
 }
